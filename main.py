f18bcc3e5797f2415a301ca82dd4d7283f090b47
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2025-02-24 14:54:28 -0500

    commit

--- main.py
+++ main.py
@@ -15,25 +15,41 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.SMOKY_BLACK)
+        self.sprite_list = arcade.SpriteList()
+        self.player_sprite = arcade.Sprite("assets/faceBeard.png", 0.3, SCREEN_WIDTH / 4, 300)
+        self.ordinateur_sprite = arcade.Sprite("assets/compy.png", 1.5, SCREEN_WIDTH * 0.75, 300)
+        self.sprite_list.append(self.player_sprite)
+        self.sprite_list.append(self.ordinateur_sprite)
 
     def on_draw(self):
         self.clear()
+        # dessine le texte ------
         title = arcade.Text("Roche, Papier, Ciseaux", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100, arcade.color.RED_BROWN,
                             70, anchor_x="center")
         rules_round_active = arcade.Text("Appuyer sur une image pour faire une attaque!", SCREEN_WIDTH / 2,
                                          SCREEN_HEIGHT - 170, arcade.color.BLIZZARD_BLUE, 40, align="center",
                                          anchor_x="center", multiline=True, width=900)
         pointage_joueur = arcade.Text("Le pointage du joueur est", SCREEN_WIDTH / 4,
-                                      SCREEN_HEIGHT - 750, arcade.color.DARK_PASTEL_GREEN, 20,
+                                      70, arcade.color.DARK_PASTEL_GREEN, 20,
                                       anchor_x="center")
-        pointage_ordinateur = arcade.Text("Le pointage de l'ordinateur est", 740,
-                                          SCREEN_HEIGHT - 750, arcade.color.BLIZZARD_BLUE, 20,
+        pointage_ordinateur = arcade.Text("Le pointage de l'ordinateur est", SCREEN_WIDTH * 0.75,
+                                          70, arcade.color.BLIZZARD_BLUE, 20,
                                           anchor_x="center")
 
         title.draw()
         rules_round_active.draw()
         pointage_joueur.draw()
         pointage_ordinateur.draw()
+
+        # dessine les cases ------
+        arcade.draw_circle_outline(SCREEN_WIDTH / 4, 170, 70, arcade.color.RED, tilt_angle=45, num_segments=4)
+        arcade.draw_circle_outline(SCREEN_WIDTH / 4 - 150, 170, 70, arcade.color.RED, tilt_angle=45, num_segments=4)
+        arcade.draw_circle_outline(SCREEN_WIDTH / 4 + 150, 170, 70, arcade.color.RED, tilt_angle=45, num_segments=4)
+        arcade.draw_circle_outline(SCREEN_WIDTH * 0.75, 170, 70, arcade.color.RED, tilt_angle=45, num_segments=4)
+
+        # dessine les sprites ------
+        self.sprite_list.draw()
+
 
 def main():
     my_game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
