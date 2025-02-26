18ae27483936f7c38c06323a48a6808b3c9b6cc3
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2025-02-26 14:51:29 -0500

    commit

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Jeu de roche papier ciseaux
 """
 
 import arcade
-import attack_animation
+from attack_animation import AttackAnimation, AttackType
+import game_state
 
 
 SCREEN_WIDTH = 1000
@@ -20,10 +21,24 @@ class MyGame(arcade.Window):
         self.sprite_list = arcade.SpriteList()
         self.player_sprite = arcade.Sprite("assets/faceBeard.png", 0.3, SCREEN_WIDTH / 4, 300)
         self.ordinateur_sprite = arcade.Sprite("assets/compy.png", 1.5, SCREEN_WIDTH * 0.75, 300)
-        self.roche_animation = attack_animation.AttackAnimation(attack_type=attack_animation.AttackType.ROCK)
+        self.roche_animation = AttackAnimation(AttackType.ROCK)
+        self.roche_animation.center_x = SCREEN_WIDTH / 4 - 150
+        self.roche_animation.center_y = 170
+        self.papier_animation = AttackAnimation(AttackType.PAPER)
+        self.papier_animation.center_x = SCREEN_WIDTH / 4
+        self.papier_animation.center_y = 170
+        self.ciseaux_animation = AttackAnimation(AttackType.SCISSORS)
+        self.ciseaux_animation.center_x = SCREEN_WIDTH / 4 + 150
+        self.ciseaux_animation.center_y = 170
+
         self.sprite_list.append(self.player_sprite)
         self.sprite_list.append(self.ordinateur_sprite)
         self.sprite_list.append(self.roche_animation)
+        self.sprite_list.append(self.papier_animation)
+        self.sprite_list.append(self.ciseaux_animation)
+
+        self.etat_jeu = game_state.GameState.NOT_STARTED
+        self.player_attack_type = ""
 
     def on_draw(self):
         self.clear()
@@ -54,8 +69,31 @@ class MyGame(arcade.Window):
         # dessine les sprites ------
         self.sprite_list.draw()
 
-    def on_update(self, delta_time: float) -> bool | None:
+    def on_update(self, delta_time: float):
         pass
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if (self.etat_jeu == game_state.GameState.NOT_STARTED
+                or self.etat_jeu == game_state.GameState.GAME_OVER
+                or self.etat_jeu == game_state.GameState.ROUND_DONE
+                and symbol == arcade.key.SPACE):
+            self.etat_jeu = game_state.GameState.ROUND_ACTIVE
+
+        if self.etat_jeu == game_state.GameState.ROUND_ACTIVE:
+            pass
+
+        print(self.etat_jeu)
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        if self.roche_animation.collides_with_point((x, y)):
+            print("vous avez toucher la roche")
+            self.player_attack_type = "Rock"
+        elif self.papier_animation.collides_with_point((x, y)):
+            print("vous avez toucher le papier")
+            self.player_attack_type = "Papier"
+        elif self.ciseaux_animation.collides_with_point((x, y)):
+            print("vous avez toucher les ciseaux")
+            self.player_attack_type = "Scisors"
 
 
 def main():
